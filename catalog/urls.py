23ac20ecb607cb0540c10895600389f62faf54df
Ольга Костенко <you@example.com>
2025-02-21 00:41:17 +0500

    В шаблоне отображаются только необходимые данные

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import home, contacts
+from catalog.views import home, contacts, one_product
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
     path('', home, name='home'),
-    path('contacts/', contacts, name='contacts')
+    path('contacts/', contacts, name='contacts'),
+    path('products/<int:pk>/', one_product, name='one_product'),
 ]
