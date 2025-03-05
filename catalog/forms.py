163f29502b5aa17dd8bef02fc06ed51ff37b0771
Ольга Костенко <you@example.com>
2025-03-05 16:09:35 +0500

    Создан класс формы для продуктов и стилизации форм

---
+++ catalog/forms.py
@@ -0,0 +1,42 @@
+from django.forms import ModelForm, BooleanField
+from django.core.exceptions import ValidationError
+
+from catalog.models import Product
+
+
+class StyleFormMixin:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for fild_name, fild in self.fields.items():
+            if isinstance(fild, BooleanField):
+                fild.widget.attrs['class'] = 'form-check-input'
+            else:
+                fild.widget.attrs['class'] = 'form-control'
+
+
+class CatalogForms(StyleFormMixin, ModelForm):
+    words = ["казино", "криптовалюта", "крипта", "биржа", "дешево", "бесплатно", "обман", "полиция", "радар"]
+
+    class Meta:
+        model = Product
+        exclude = ("created_at", "updated_at")
+
+    def clean_name(self):
+        name = self.cleaned_data.get('name')
+        for word in self.words:
+            if word in name.lower():
+                raise ValidationError(f'В названии продукта неможет быть слова {word}')
+        return name
+
+    def clean_description(self):
+        description = self.cleaned_data.get('description')
+        for word in self.words:
+            if word in description.lower():
+                raise ValidationError(f'В описании продукта неможет быть слова {word}')
+        return description
+
+    def clean_price(self):
+        price = self.cleaned_data['price']
+        if price < 0:
+            raise ValidationError('Цена продукта не может быть отрицательной.')
+        return price
