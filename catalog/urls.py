c6af9194c5a7c9dbe2766415be0239a594527296
Ольга Костенко <you@example.com>
2025-03-05 16:13:41 +0500

    Все необходимые контроллеры зарегистрированы

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import CatalogListView, CatalogTemplateView, CatalogDetailView
+from catalog.views import CatalogListView, CatalogTemplateView, CatalogDetailView, CatalogCreateView, CatalogUpdateView, CatalogDeleteView
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', CatalogListView.as_view(), name='home'),
+    path('', CatalogListView.as_view(), name='product_list'),
     path('contacts/', CatalogTemplateView.as_view(), name='contacts'),
-    path('products/<int:pk>/', CatalogDetailView.as_view(), name='one_product'),
+    path('products/<int:pk>/', CatalogDetailView.as_view(), name='product_detail'),
+    path('products/create/', CatalogCreateView.as_view(), name='product_create'),
+    path('product/<int:pk>/update', CatalogUpdateView.as_view(), name='product_update'),
+    path('product/<int:pk>/delete', CatalogDeleteView.as_view(), name='product_delete')
 ]
